5355825f22e3a5d454f0968d38041d7662957385
Paul Anthony McGowan <paul.mcgowan@kingsbridge.co.uk>
2021-12-13 13:29:11 +0000

    Update Email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,5 +83,5 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.disable_request_forgery_protection = true
 end
---
+++ config/routes/madmin.rb
@@ -0,0 +1,17 @@
+# Below are the routes for madmin
+namespace :madmin do
+  resources :users
+  namespace :active_storage do
+    resources :attachments
+  end
+  namespace :active_storage do
+    resources :variant_records
+  end
+  namespace :active_storage do
+    resources :blobs
+  end
+  resources :services
+  resources :announcements
+  resources :notifications
+  root to: "dashboard#show"
+end
